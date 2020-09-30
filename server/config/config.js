ede9cdcd4e687c92b2f935ebe9bd0c9b10c7f1ba
Jhon <jhon.charryserrato@gmail.com>
2020-09-30 16:06:50 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://jhon_325:AidBcZ95QTiSPGF5@cluster0.cqkam.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 
